0e66bd6c46b11693761258e74ec693744b63ddf5
foruser <vinhpham2761.gmail.com>
2024-05-10 11:29:13 +0700

    comment

---
+++ Comment/comment.c
@@ -0,0 +1,25 @@
+/**
+ * FILENAME: comment.c
+ * DESCRIPTION: This program use to understand comment
+ * AUTHOR: 4USER
+ * DATE: 2021-09-19
+ * MODIFIED: FOR USER
+ * CHANGES: .....
+*/
+
+#include <stdio.h>
+
+int main() {
+    // day la doan code dung de comment cho ngon ngu C, va the hien ro suc manh cua ham printf thuoc thu vien stdio.h
+    /*
+    hello
+    abc
+    xyz
+    ggg
+    vcxvxcv
+    vxcvxcvxcvxc
+    vxcvxc
+    */
+    printf("Hello, World!\n");// print hello world
+    return 0;
+}
---
+++ Comment/comment.md
@@ -0,0 +1,16 @@
+# Single-line Comments
++ single line comment bat dau bang dau "//" 
++ tuong dong voi ngon ngu java
++ compiler bo qua dong comment
+
+# Cach comment
++ comment ngay phia truoc dong code
++ comment ngay ben canh dong code
+
+# Multi-line comment
++ comment se nam trong cap dau "/* */"
+
+# Header comment
++ tao ra nhyung header comment o tren dau file
++ xac dinh author code, ngay thang chung ta viet code, sua code, xoa code
++ xac dinh mieu ta cua nhung cai lna thay doi do la gi
